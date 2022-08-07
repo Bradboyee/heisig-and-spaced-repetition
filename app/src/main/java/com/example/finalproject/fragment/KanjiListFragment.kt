3582722afbee315637a9f22dfde442cc26964e10
Bradboyee <96470193+Bradboyee@users.noreply.github.com>
2022-08-08 01:42:24 +0700

    use Navigation Component

---
+++ app/src/main/java/com/example/finalproject/fragment/KanjiListFragment.kt
@@ -0,0 +1,28 @@
+package com.example.finalproject.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.finalproject.data_kanji.Data
+import com.example.finalproject.databinding.FragmentKanjiListBinding
+import com.example.finalproject.utils.KanjiRecyclerAdapter
+
+class KanjiListFragment : Fragment() {
+    private var _binding: FragmentKanjiListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentKanjiListBinding.inflate(inflater,container,false)
+        val categoricalValue = arguments?.getInt("amount")
+        binding.kanjiListRecyclerView.layoutManager = GridLayoutManager(this.context,5)
+        binding.kanjiListRecyclerView.adapter = KanjiRecyclerAdapter(Data.kanji,categoricalValue!!)
+        return binding.root
+    }
+}
